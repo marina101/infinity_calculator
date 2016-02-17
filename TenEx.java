c5c445deae19d6f5df8245ef6d2d13078071c74b
marina101 <mchirc1@gmail.com>
2016-02-17 17:25:59 -0500

    cleaned up, testing passed

--- TenEx.java
+++ TenEx.java
@@ -1,44 +1,46 @@
-import java.util.LinkedList;
-import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-public class TenEx implements IAlgorithm {
+
+/**
+ * This class is for calculating the value of 10^x using the algorithm 10^x = e^(x*ln10)
+ * It does not rely on the java.math class, but instead only uses methods created for this project
+ * It effectively calculates 10^x with 6 decimals of precision
+ * @author Marina Chirchikova
+ *
+ */
+public class TenEx{ //note I took away "implements IA" for now since now my
+	//method only takes one double value, which is different from IA
 	
 	private static final double LN10 = 2.3025850929940457;
 	
-	//note: need to change method in IAlgorithm interface to static as well!
-	//note 2 for refactoring: method names should not be capitalized
+	//preconditions: -assumes method in IAlgorithm interface is static
+	//				 -assumes naturalExp method is public
+	//				-takes only one double
 	
-	public static double Calculate(List<Double> values){
-		double number = values.get(0);
-		double result = Math.exp(number * LN10);
+	/**
+	 * This method calculates 10^x, using algorithm 10^x = e^(x*ln10)
+	 * @param value of x
+	 * @return value of 10^x
+	 */
+	public static double Calculate(double value){
+		double result = NaturalExponential.naturalExp(value * LN10);
 		return result;
 	}
 	
-	public static double Calculate2(List<Double> values){
-		double number = values.get(0);
-		//need to change naturalExp to public
-		double result2 = NaturalExponential.naturalExp(number * LN10);
-		return result2;
+	//these test compares the value coming from the calculate method with the 
+	//values that webcalculator 2.0 gives
+	@Test
+	public void testZero(){
+		//testing when value is 0
+		assertEquals(1, Calculate(0), 0.0);
 	}
 	
-	public static void main(String[] args){
-		
-		
-		Double var1 = (double) 6.857;
-		//Double var2 = (double) 2;
-		Double expectedResult = (double) 7194489.780036994; //taken from a web scientific calc
-		
-		List<Double> values = new LinkedList<Double>();
-		values.add(var1);
-		//values.add(var2);
-		System.out.println("number is " + values.get(0));
-		double result = TenEx.Calculate(values);
-		
-		System.out.println("Actual result is " + result + " and expected result is " + expectedResult);
-		
-		System.out.println("And now using the natural exponent class");
-		double result2 = TenEx.Calculate2(values);
-		System.out.println("Actual result using homemade e^x method is "+ result);
-		
+	@Test
+	public void testNumber(){
+		//testing when value is 6.857, if precision is <= 6 decimal points, the test passes
+		assertEquals(7194489.780037, Calculate(6.857), 0.000001);
 	}
+	 
 }
+
