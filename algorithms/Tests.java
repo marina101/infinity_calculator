b23f52a76271e27962040aff29453b17b2c4fc9c
Trajan64 <je.charriere@gmail.com>
2016-02-22 00:02:00 -0500

    Update Tests.java

--- algorithms/Tests.java
+++ algorithms/Tests.java
@@ -1,8 +1,4 @@
 import static org.junit.Assert.*;
-
-import java.util.LinkedList;
-import java.util.List;
-
 import org.junit.Test;
 
 public class Tests {
@@ -10,28 +6,33 @@ public class Tests {
 	@Test
 	public void test() {
 		
-		double errorMargin = 0.000000003;
+		/* We specify an error margin for each of the test functions.
+		 * 
+		 * The error margins chosen demonstrate the precision limits of each of the functions. 
+		 * Anything beyond those values will trigger a test failure. 
+		 */
 		
-		testSin(errorMargin);
-		testNatExp(errorMargin);
-		testPower(errorMargin);
-		testTenEx(errorMargin);
-		testNatLog(errorMargin);
-		testStdDeviation(errorMargin);
+		testSin(	0.000000000001);
+		testExp(	0.00000000001);
+		testPower(	0.00000000000001);
+		testTenEx(	0.00001);
+		testNatLog(	0.0000000001);
+		testSqrt(	0.0000000001);
 		
 	}
 	
 	
 	public void testSin(double delta) {
 					
-		assertEquals(0.0, 			Algorithms.sin(0.0), 	delta);
-		assertEquals(-0.98803162, 	Algorithms.sin(30.0), 	delta);
+		assertEquals(0.0, 					Algorithms.sin(0.0), 	delta);
+		assertEquals(-0.9880316240928618, 	Algorithms.sin(30.0), 	delta);
 		
 	}
 	
-	public void testNatExp(double delta) {
+	public void testExp(double delta) {
 		
-		assertEquals(0.0, 			Algorithms.natExp(0.0), 	delta);
+		assertEquals(1, 							Algorithms.exp(0), 	delta);		
+		assertEquals(2980.9579870417283, 			Algorithms.exp(8), 	delta);
 
 		
 	}
@@ -39,7 +40,7 @@ public class Tests {
 	public void testPower(double delta) {
 		
 		assertEquals(1.0, 				Algorithms.power(0.0, 0.0), 	delta);
-		assertEquals(197798520.1462557, Algorithms.power(3.3, 16.0), 	delta);
+		assertEquals(22.54218602980021, Algorithms.power(2.5, 3.4), 	delta);
 		
 	}
 	
@@ -52,31 +53,16 @@ public class Tests {
 	
 	public void testNatLog(double delta) {
 		
-		assertEquals(0.0, 			Algorithms.natLog(0.0), 	delta);
-		assertEquals(3.401197382, 	Algorithms.natLog(30.0), 	delta);
+		assertEquals(6.2971093199, 			Algorithms.natLog(543), 	delta);
+		assertEquals(3.4011973816621555, 	Algorithms.natLog(30.0), 	delta);
 		
 	}
 	
 	
-	public void testStdDeviation(double delta) {
-		
-		
-		double var1 = (double) 10;
-		double var2 = (double) 20;
-		double var3 = (double) 34;
-		double var4 = (double) 56;
-		double var5 = (double) 76;
-		double var6 = (double) 54;
-		
-		List<Double> values = new LinkedList<Double>();
-		values.add(var1);
-		values.add(var2);
-		values.add(var3);
-		values.add(var4);
-		values.add(var5);
-		values.add(var6);
-		
-		assertEquals(22.61, Algorithms.stdDeviation(values), delta);
+	public void testSqrt(double delta) {
+
+		assertEquals(0, 				Algorithms.sqrt(0), delta);
+		assertEquals(1.224744871391589, Algorithms.sqrt(1.5), delta);
 		
 	}
 	
