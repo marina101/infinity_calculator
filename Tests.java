82eaf81c76fe228348f2e971c278211d88d18a94
Trajan64 <je.charriere@gmail.com>
2016-02-22 21:01:56 -0500

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -13,10 +13,10 @@ public class Tests {
 		 */
 		
 		testSin(	0.000000000001);
-		testExp(	0.00000000001);
+		testExp(	0.00000000000001);
 		testPower(	0.00000000000001);
-		testTenEx(	0.00001);
-		testNatLog(	0.0000000001);
+		testTenEx(	0.00000000001);
+		testNatLog(	0.00000000001);
 		testSqrt(	0.0000000001);
 		
 	}
@@ -24,45 +24,45 @@ public class Tests {
 	
 	public void testSin(double delta) {
 					
-		assertEquals(0.0, 					Algorithms.sin(0.0), 	delta);
-		assertEquals(-0.9880316240928618, 	Algorithms.sin(30.0), 	delta);
+		assertEquals(Math.sin(0), 					Algorithms.sin(0), 	delta);
+		assertEquals(Math.sin(Math.PI), 			Algorithms.sin(Math.PI), 	delta);
 		
 	}
 	
 	public void testExp(double delta) {
 		
-		assertEquals(1, 							Algorithms.exp(0), 	delta);		
-		assertEquals(2980.9579870417283, 			Algorithms.exp(8), 	delta);
+		assertEquals(Math.exp(0), 							Algorithms.exp(0), 	delta);		
+		assertEquals(Math.exp(Math.E), 						Algorithms.exp(Math.E), 	delta);
 
 		
 	}
 	
 	public void testPower(double delta) {
 		
-		assertEquals(1.0, 				Algorithms.power(0.0, 0.0), 	delta);
-		assertEquals(22.54218602980021, Algorithms.power(2.5, 3.4), 	delta);
+		assertEquals(Math.pow(0, 0), 						Algorithms.power(0, 0), 	delta);
+		assertEquals(Math.pow(Math.sqrt(6), Math.sqrt(2)), 	Algorithms.power(Math.sqrt(6), Math.sqrt(2)), 	delta);
 		
 	}
 	
 	public void testTenEx(double delta) {
 		
-		assertEquals(1.0, 				Algorithms.tenEx(0.0), 	delta);
-		assertEquals(1000000000, 		Algorithms.tenEx(9.0), 	delta);
+		assertEquals(Math.pow(10, 0), 				Algorithms.tenEx(0), 	delta);
+		assertEquals(Math.pow(10, Math.PI), 		Algorithms.tenEx(Math.PI), 	delta);
 		
 	}
 	
 	public void testNatLog(double delta) {
 		
-		assertEquals(6.2971093199, 			Algorithms.natLog(543), 	delta);
-		assertEquals(3.4011973816621555, 	Algorithms.natLog(30.0), 	delta);
+		assertEquals(Math.log(543), 			Algorithms.natLog(543), 	delta);
+		assertEquals(Math.log(Math.E), 			Algorithms.natLog(Math.E), 	delta);
 		
 	}
 	
 	
 	public void testSqrt(double delta) {
 
-		assertEquals(0, 				Algorithms.sqrt(0), delta);
-		assertEquals(1.224744871391589, Algorithms.sqrt(1.5), delta);
+		assertEquals(Math.sqrt(0), 				Algorithms.sqrt(0), delta);
+		assertEquals(Math.sqrt(Math.E), 		Algorithms.sqrt(Math.E), delta);
 		
 	}
 	
